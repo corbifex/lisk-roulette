b2f3565f1853bfe0f78211b5361e0276d6612835
raphael <raphael@moosty.com>
2019-10-15 22:53:52 +0200

    button

--- client/src/components/table/account.js
+++ client/src/components/table/account.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import './account.css';
   import Button from '@material-ui/core/Button';
+  import Grid from '@material-ui/core/Grid';
+  import ButtonGroup from '@material-ui/core/ButtonGroup';
+  import CheckIcon from '@material-ui/icons/Check';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+
+
+
 
 export class Account extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {saldo: 122323, current: 50, confirmed: "(unconfirmed)"};
+    this.state = {saldo: 0, current: 0, confirmed: "(unconfirmed)", setSelected: false, selected: false};
   }
 
   confirm() {
     this.setState({confirmed: "(Confirmed)"});
   }
 
+  StandaloneToggleButton() {
+  const [selected, setSelected] = React.useState(true);
+}
+
   render() {
     return (
       <div className="Account-container">
-        <div className="Saldo">Total:{"\n"}
-          {this.state.saldo} Tokens</div>
-        <div className="Current-bet">Current Bet:
-          {"\n"} {this.props.currentBet} Tokens {this.state.confirmed}</div>
-        <Button variant="contained" color="primary" onClick={this.confirm.bind(this)}>
-           Confirm bet
-        </Button>
+        <div className="Saldo">Total: {this.state.saldo} Tokens</div>
+          <div className="Current-bet">Current Bet: {this.props.currentBet} Tokens </div>
+            
+              
+                <Grid item xs={12} md={6}>  
+        <ButtonGroup fullWidth aria-label="full width secondary contained button group">
+          <Button className="active" onClick={this.confirm.bind(this)}>Confirm</Button>
+          <Button onClick={this.confirm.bind(this)}>Repeat</Button>
+          <Button onClick={this.confirm.bind(this)}>Clear</Button>
+        </ButtonGroup>
+      </Grid>
+ 
 
       </div>
     );
