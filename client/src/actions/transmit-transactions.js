8198eabd129c04d21ae57532cafe249181fc9c84
Corbifex <sander@moosty.com>
2019-10-25 19:47:05 +0200

    https

--- client/src/actions/transmit-transactions.js
+++ client/src/actions/transmit-transactions.js
@@ -2,7 +2,7 @@ const { APIClient } = require('@liskhq/lisk-api-client');
 
 export class TransmitTransactions {
   constructor() {
-    this.APIClient = new APIClient(['http://api.roulette.delegate.moosty.com']);
+    this.APIClient = new APIClient(['https://api.roulette.delegate.moosty.com']);
   }
 
   send(transaction) {
